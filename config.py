fa6fab5dd30ffa05a6d4e9d25baa2b2a5f6b5851
Spizzicat <jiskwright@gmail.com>
2023-12-01 19:23:46 -0500

    changes to stuff

--- config.py
+++ config.py
@@ -18,21 +18,3 @@ def write_bank(bank):
     with open(f'{DIR}/data/bank.json','w') as file:
         json.dump(bank, file, indent=4)
     file.close()
-
-# def csc(x):
-#     if plt.sin(x) != 0:
-#         return 1/plt.sin(x)
-#     else:
-#         return None
-
-# def sec(x):
-#     if plt.cos(x) != 0:
-#         return 1/plt.cos(x)
-#     else:
-#         return None
-
-# def cot(x):
-#     if plt.tan(x) != 0:
-#         return 1/plt.tan(x)
-#     else:
-#         return None
